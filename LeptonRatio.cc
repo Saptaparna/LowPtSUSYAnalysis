93a81be2bf38fe58c324c8bbd92a2bef76531036
Saptaparna Bhattacharya <saptaparna@gmail.com>
2015-02-13 09:29:55 -0500

    Latest version of ratio code; some lines commented out for simplicity

--- LeptonRatio.cc
+++ LeptonRatio.cc
@@ -5,6 +5,7 @@
 #include"FWCore/Framework/interface/EDAnalyzer.h"
 #include"FWCore/Framework/interface/Event.h"
 #include"DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
+#include"TLorentzVector.h"
 
 LeptonRatio::LeptonRatio(const edm::ParameterSet& iConfig)
 
@@ -13,85 +14,219 @@ LeptonRatio::LeptonRatio(const edm::ParameterSet& iConfig)
     electrons=0;
     muons=0;
     taus=0;
+    nues=0;
+    numus=0;
+    nutaus=0;
     n_dielectrons=0;
     n_dimuons=0;
     n_ditaus=0;
     n_dileptons=0;
     n_electronTau=0;
     n_muonTau=0;
+    Wmass=0;
+    NeutralinoMass=0;
+    CharginoMass=0;
+    Ws=0;
 }
 
 
 LeptonRatio::~LeptonRatio()
 {
  
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-
 }
 
-
-//
-// member functions
-//
-
-// ------------ method called for each event  ------------
 void
 LeptonRatio::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
    using namespace edm;
    Handle<std::vector<reco::GenParticle> > genParticles;
    iEvent.getByLabel("genParticles", genParticles) ;
-
    int n_electrons=0;
    int n_muons=0;
    int n_taus=0;
+   int n_nues=0;
+   int n_numus=0;
+   int n_nutaus=0;
+   int n_Ws=0;
+   genElectronPt.clear();
+   genElectronEta.clear();
+   genElectronPhi.clear();
+   genElectronEnergy.clear();
+   genMuonPt.clear();
+   genMuonEta.clear();
+   genMuonPhi.clear();
+   genMuonEnergy.clear();
+   genTauPt.clear();
+   genTauEta.clear();
+   genTauPhi.clear();
+   genTauEnergy.clear();
+   genElectronNeutrinoPt.clear();
+   genElectronNeutrinoEta.clear();
+   genElectronNeutrinoPhi.clear();
+   genElectronNeutrinoEnergy.clear(); 
+   genMuonNeutrinoPt.clear();
+   genMuonNeutrinoEta.clear();
+   genMuonNeutrinoPhi.clear();
+   genMuonNeutrinoEnergy.clear();
+   genTauNeutrinoPt.clear();
+   genTauNeutrinoEta.clear();
+   genTauNeutrinoPhi.clear();
+   genTauNeutrinoEnergy.clear();
+   genNeutralinoPt.clear();
+   genNeutralinoEta.clear();
+   genNeutralinoPhi.clear();
+   genNeutralinoEnergy.clear();
+   genCharginoPt.clear();
+   genCharginoEta.clear();
+   genCharginoPhi.clear();
+   genCharginoEnergy.clear();
+   genWId_El.clear();
+   genWPt_El.clear();
+   genWEta_El.clear();
+   genWPhi_El.clear();   
+   genWEnergy_El.clear();
+   genWId_Mu.clear();
+   genWPt_Mu.clear();
+   genWEta_Mu.clear();
+   genWPhi_Mu.clear();
+   genWEnergy_Mu.clear();
+   genWId_Tau.clear();
+   genWPt_Tau.clear();
+   genWEta_Tau.clear();
+   genWPhi_Tau.clear();
+   genWEnergy_Tau.clear();
+
    for(std::vector<reco::GenParticle>::const_iterator genParticle = genParticles->begin(); genParticle != genParticles->end(); genParticle++){
-    int id = genParticle->pdgId();
-    int st = genParticle->status();
-    int mother_id = 0;
-    if( genParticle->mother()) mother_id = genParticle->mother()->pdgId();
-    double pt = genParticle->pt();
-    if (abs(id)==11 && st==3 and pt>0.5 and abs(mother_id)==24){
+     int id = genParticle->pdgId();
+     int st = genParticle->status(); 
+     int mother_id = 0;
+     if(genParticle->mother()) mother_id = genParticle->mother()->pdgId();
+     
+     if(abs(id)==11 and st==3){// and abs(mother_id)==24){ 
        electrons++;
        n_electrons++;
-                }
+       genElectronPt.push_back(genParticle->pt()); 
+       genElectronEta.push_back(genParticle->eta());
+       genElectronPhi.push_back(genParticle->phi());
+       genElectronEnergy.push_back(genParticle->energy());
+       int nudaughter_id = -1;
+       /*if(abs(genParticle->mother()->daughter(0)->pdgId()) == 12) nudaughter_id=0;
+       else if(abs(genParticle->mother()->daughter(1)->pdgId()) == 12) nudaughter_id=1;
+       if (nudaughter_id==-1) std::cout<<"No neutrino daughter found!"<<std::endl;
+       else{
+         genElectronNeutrinoPt.push_back(genParticle->mother()->daughter(nudaughter_id)->pt());
+         genElectronNeutrinoEta.push_back(genParticle->mother()->daughter(nudaughter_id)->eta());
+         genElectronNeutrinoPhi.push_back(genParticle->mother()->daughter(nudaughter_id)->phi());
+         genElectronNeutrinoEnergy.push_back(genParticle->mother()->daughter(nudaughter_id)->energy());
+       }
+       if(genParticle->mother()->mother()) genWId_El.push_back(genParticle->mother()->mother()->pdgId());
+       genWPt_El.push_back(genParticle->mother()->pt());
+       genWEta_El.push_back(genParticle->mother()->eta());
+       genWPhi_El.push_back(genParticle->mother()->phi());
+       genWEnergy_El.push_back(genParticle->mother()->energy());
+       std::cout<<"electron + neutrino energy = "<<genParticle->energy() + genParticle->mother()->daughter(nudaughter_id)->energy() <<"; W energy = "<<genParticle->mother()->energy()<<std::endl;*/
+     }
 
-    if (abs(id)==13 && st==3 and pt>0.5 and abs(mother_id)==24){
-        muons++;
-        n_muons++;
-                }
+    if(abs(id)==13 and st==3){// and abs(mother_id)==24){
+       muons++;
+       n_muons++;
+       genMuonPt.push_back(genParticle->pt());
+       genMuonEta.push_back(genParticle->eta());
+       genMuonPhi.push_back(genParticle->phi());
+       genMuonEnergy.push_back(genParticle->energy());
+       int nudaughter_id = -1;
+  /*     if(abs(genParticle->mother()->daughter(0)->pdgId()) == 14) nudaughter_id=0;
+       else if(abs(genParticle->mother()->daughter(1)->pdgId()) == 14) nudaughter_id=1;
+       if (nudaughter_id==-1) std::cout<<"No neutrino daughter found!"<<std::endl;
+       else{
+         genMuonNeutrinoPt.push_back(genParticle->mother()->daughter(nudaughter_id)->pt());
+         genMuonNeutrinoEta.push_back(genParticle->mother()->daughter(nudaughter_id)->eta());
+         genMuonNeutrinoPhi.push_back(genParticle->mother()->daughter(nudaughter_id)->phi());
+         genMuonNeutrinoEnergy.push_back(genParticle->mother()->daughter(nudaughter_id)->energy());
+       }
+       if(genParticle->mother()->mother()) genWId_Mu.push_back(genParticle->mother()->mother()->pdgId());
+       genWPt_Mu.push_back(genParticle->mother()->pt());
+       genWEta_Mu.push_back(genParticle->mother()->eta());
+       genWPhi_Mu.push_back(genParticle->mother()->phi());
+       genWEnergy_Mu.push_back(genParticle->mother()->energy());
+       std::cout<<"muon + neutrino energy = "<<genParticle->energy() + genParticle->mother()->daughter(nudaughter_id)->energy() <<"; W energy = "<<genParticle->mother()->energy()<<std::endl;*/
+     }    
 
-    if (abs(id)==15 && st==3 and pt>0.5 and abs(mother_id)==24){
+    if(abs(id)==15 and st==3){// and abs(mother_id)==24){
        taus++;
        n_taus++;
+       genTauPt.push_back(genParticle->pt());
+       genTauEta.push_back(genParticle->eta());
+       genTauPhi.push_back(genParticle->phi());
+       genTauEnergy.push_back(genParticle->energy());
+       int nudaughter_id = -1;
+       /*if(abs(genParticle->mother()->daughter(0)->pdgId()) == 16) nudaughter_id=0;
+       else if(abs(genParticle->mother()->daughter(1)->pdgId()) == 16) nudaughter_id=1;
+       if (nudaughter_id==-1) std::cout<<"No neutrino daughter found!"<<std::endl;
+       else{
+         genTauNeutrinoPt.push_back(genParticle->mother()->daughter(nudaughter_id)->pt());
+         genTauNeutrinoEta.push_back(genParticle->mother()->daughter(nudaughter_id)->eta());
+         genTauNeutrinoPhi.push_back(genParticle->mother()->daughter(nudaughter_id)->phi());
+         genTauNeutrinoEnergy.push_back(genParticle->mother()->daughter(nudaughter_id)->energy());
        }
+       if(genParticle->mother()->mother()) genWId_Tau.push_back(genParticle->mother()->mother()->pdgId());
+       genWPt_Tau.push_back(genParticle->mother()->pt());
+       genWEta_Tau.push_back(genParticle->mother()->eta());
+       genWPhi_Tau.push_back(genParticle->mother()->phi());
+       genWEnergy_Tau.push_back(genParticle->mother()->energy());
+       std::cout<<"tau + neutrino energy = "<<genParticle->energy() + genParticle->mother()->daughter(nudaughter_id)->energy() <<"; W energy = "<<genParticle->mother()->energy()<<std::endl;
+*/
     }
+/*
+    if(abs(id)==1000022 and st==3 and abs(mother_id)==1000024){
+       genNeutralinoPt.push_back(genParticle->pt());
+       genNeutralinoEta.push_back(genParticle->eta());
+       genNeutralinoPhi.push_back(genParticle->phi());
+       genNeutralinoEnergy.push_back(genParticle->energy());
+      }
 
-    if(n_electrons==2 && n_muons==0 && n_taus==0){
+    if(abs(id)==1000024 and st==3 and abs(mother_id)==1000006){
+       genCharginoPt.push_back(genParticle->pt());
+       genCharginoEta.push_back(genParticle->eta());
+       genCharginoPhi.push_back(genParticle->phi());
+       genCharginoEnergy.push_back(genParticle->energy());
+       } */
+     }
+   if(n_electrons==2 && n_muons==0 && n_taus==0){
        n_dielectrons++;
        }
 
-    if(n_muons==2 && n_electrons==0 && n_taus==0){
+     if(n_muons==2 && n_electrons==0 && n_taus==0){
        n_dimuons++;
        }
 
-    if(n_electrons==1 && n_muons==1 && n_taus==0){
+     if(n_electrons==1 && n_muons==1 && n_taus==0){
        n_dileptons++;
        }
     
-    if(n_taus==2 && n_muons==0 && n_electrons==0){
+     if(n_taus==2 && n_muons==0 && n_electrons==0){
        n_ditaus++;
        }
     
-    if(n_electrons==1 && n_taus==1 && n_muons==0){
+     if(n_electrons==1 && n_taus==1 && n_muons==0){
        n_electronTau++;
        } 
      
-    if(n_electrons==0 && n_taus==1 && n_muons==1){
+     if(n_electrons==0 && n_taus==1 && n_muons==1){
        n_muonTau++;
        }    
-
+/*
+   TLorentzVector leadingEl;
+   TLorentzVector leadingElNu;
+   if(genElectronPt.size() > 0) leadingEl.SetPtEtaPhiE(genElectronPt[0], genElectronEta[0], genElectronPhi[0], genElectronEnergy[0]);
+   if(genElectronNeutrinoPt.size() > 0) leadingElNu.SetPtEtaPhiE(genElectronNeutrinoPt[0], genElectronNeutrinoEta[0], genElectronNeutrinoPhi[0], genElectronNeutrinoEnergy[0]);
+   Wmass = (leadingEl+leadingElNu).M();
+   TLorentzVector neutralino;
+   if(genNeutralinoPt.size()>0.0) neutralino.SetPtEtaPhiE(genNeutralinoPt[0], genNeutralinoEta[0], genNeutralinoPhi[0], genNeutralinoEnergy[0]);
+   NeutralinoMass = neutralino.M();
+   std::cout << "NeutralinoMass = " << NeutralinoMass << std::endl;
+   CharginoMass = (leadingEl+leadingElNu+neutralino).M();*/
+    _tree->Fill();
 }
 
 
@@ -99,6 +234,50 @@ LeptonRatio::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 void 
 LeptonRatio::beginJob()
 {
+ _file = new TFile("LeptonProperties_GenLevel.root", "RECREATE");
+ _tree = new TTree("LeptonProperties_GenLevel", "LeptonProperties_GenLevel", 64000000);
+/* _branch = _tree->Branch("genElectronPt", &genElectronPt);
+ _branch = _tree->Branch("genElectronEta", &genElectronEta);
+ _branch = _tree->Branch("genElectronPhi", &genElectronPhi);
+ _branch = _tree->Branch("genElectronEnergy", &genElectronEnergy);
+ _branch = _tree->Branch("genElectronNeutrinoPt", &genElectronNeutrinoPt);
+ _branch = _tree->Branch("genElectronNeutrinoEta", &genElectronNeutrinoEta);
+ _branch = _tree->Branch("genElectronNeutrinoPhi", &genElectronNeutrinoPhi);
+ _branch = _tree->Branch("genElectronNeutrinoEnergy", &genElectronNeutrinoEnergy);
+ _branch = _tree->Branch("genMuonPt", &genMuonPt);
+ _branch = _tree->Branch("genMuonEta", &genMuonEta);
+ _branch = _tree->Branch("genMuonPhi", &genMuonPhi);
+ _branch = _tree->Branch("genMuonEnergy", &genMuonEnergy);
+ _branch = _tree->Branch("genMuonNeutrinoPt", &genMuonNeutrinoPt);
+ _branch = _tree->Branch("genMuonNeutrinoEta", &genMuonNeutrinoEta);
+ _branch = _tree->Branch("genMuonNeutrinoPhi", &genMuonNeutrinoPhi);
+ _branch = _tree->Branch("genMuonNeutrinoEnergy", &genMuonNeutrinoEnergy);
+ _branch = _tree->Branch("genTauPt", &genTauPt);
+ _branch = _tree->Branch("genTauEta", &genTauEta);
+ _branch = _tree->Branch("genTauPhi", &genTauPhi);
+ _branch = _tree->Branch("genTauEnergy", &genTauEnergy);
+ _branch = _tree->Branch("genTauNeutrinoPt", &genTauNeutrinoPt);
+ _branch = _tree->Branch("genTauNeutrinoEta", &genTauNeutrinoEta);
+ _branch = _tree->Branch("genTauNeutrinoPhi", &genTauNeutrinoPhi);
+ _branch = _tree->Branch("genTauNeutrinoEnergy", &genTauNeutrinoEnergy);
+ _branch = _tree->Branch("genWId_El", &genWId_El);
+ _branch = _tree->Branch("genWPt_El", &genWPt_El);
+ _branch = _tree->Branch("genWEta_El", &genWEta_El);
+ _branch = _tree->Branch("genWPhi_El", &genWPhi_El);
+ _branch = _tree->Branch("genWEnergy_El", &genWEnergy_El);
+ _branch = _tree->Branch("genWId_Mu", &genWId_Mu);
+ _branch = _tree->Branch("genWPt_Mu", &genWPt_Mu);
+ _branch = _tree->Branch("genWEta_Mu", &genWEta_Mu);
+ _branch = _tree->Branch("genWPhi_Mu", &genWPhi_Mu);
+ _branch = _tree->Branch("genWEnergy_Mu", &genWEnergy_Mu);
+ _branch = _tree->Branch("genWId_Tau", &genWId_Tau);
+ _branch = _tree->Branch("genWPt_Tau", &genWPt_Tau);
+ _branch = _tree->Branch("genWEta_Tau", &genWEta_Tau);
+ _branch = _tree->Branch("genWPhi_Tau", &genWPhi_Tau);
+ _branch = _tree->Branch("genWEnergy_Tau", &genWEnergy_Tau);
+ _branch = _tree->Branch("Wmass", &Wmass, "Wmass/D");
+ _branch = _tree->Branch("NeutralinoMass", &NeutralinoMass, "NeutralinoMass/D");
+ _branch = _tree->Branch("CharginoMass", &CharginoMass, "CharginoMass/D");*/
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
@@ -114,6 +293,9 @@ std::cout << "n_dileptons = " << n_dileptons << std::endl;
 std::cout << "n_ditaus = " << n_ditaus  << std::endl;
 std::cout << "n_electronTau = " << n_electronTau << std::endl;
 std::cout << "n_muonTau = " << n_muonTau << std::endl;
+_file->Write();
+_file->Close();
+
 }
 
 // ------------ method called when starting to processes a run  ------------
--- LeptonRatio.h
+++ LeptonRatio.h
@@ -29,6 +29,16 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include"TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include <TChain.h>
+#include <TLorentzVector.h>
+#include <TLegend.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
 //
 // class declaration
 //
@@ -44,16 +54,84 @@ class LeptonRatio : public edm::EDAnalyzer {
       virtual void beginJob() ;
       virtual void analyze(const edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
+      TFile * _file;
+      TTree * _tree;
+      TBranch * _branch;
       int electrons;
       int muons;
       int taus;
+      int nues;
+      int numus;
+      int nutaus;
       int n_dielectrons;
       int n_dimuons;
       int n_dileptons;
       int n_ditaus;
       int n_muonTau;
       int n_electronTau;
+      int Ws;
+      int n_Ws;
       double pt;
+      double Wmass;
+      double NeutralinoMass;
+      double CharginoMass;
+      std::vector<double> genWId_El; 
+      std::vector<double> genWPt_El;
+      std::vector<double> genWEta_El;
+      std::vector<double> genWPhi_El;
+      std::vector<double> genWEnergy_El;
+    
+      std::vector<double> genWId_Mu;
+      std::vector<double> genWPt_Mu;
+      std::vector<double> genWEta_Mu;
+      std::vector<double> genWPhi_Mu;
+      std::vector<double> genWEnergy_Mu;
+
+      std::vector<double> genWId_Tau;
+      std::vector<double> genWPt_Tau;
+      std::vector<double> genWEta_Tau;
+      std::vector<double> genWPhi_Tau;
+      std::vector<double> genWEnergy_Tau;
+
+      std::vector<double> genElectronPt;
+      std::vector<double> genElectronEta;
+      std::vector<double> genElectronPhi; 
+      std::vector<double> genElectronEnergy;
+
+      std::vector<double> genMuonPt;
+      std::vector<double> genMuonEta;
+      std::vector<double> genMuonPhi;
+      std::vector<double> genMuonEnergy;
+
+      std::vector<double> genTauPt;
+      std::vector<double> genTauEta;
+      std::vector<double> genTauPhi;
+      std::vector<double> genTauEnergy;
+
+      std::vector<double> genElectronNeutrinoPt;
+      std::vector<double> genElectronNeutrinoEta;
+      std::vector<double> genElectronNeutrinoPhi;
+      std::vector<double> genElectronNeutrinoEnergy;
+
+      std::vector<double> genMuonNeutrinoPt;
+      std::vector<double> genMuonNeutrinoEta;
+      std::vector<double> genMuonNeutrinoPhi;
+      std::vector<double> genMuonNeutrinoEnergy;
+      
+      std::vector<double> genTauNeutrinoPt;
+      std::vector<double> genTauNeutrinoEta;
+      std::vector<double> genTauNeutrinoPhi;
+      std::vector<double> genTauNeutrinoEnergy;
+
+      std::vector<double> genNeutralinoPt;
+      std::vector<double> genNeutralinoEta;
+      std::vector<double> genNeutralinoPhi;
+      std::vector<double> genNeutralinoEnergy;
+
+      std::vector<double> genCharginoPt;
+      std::vector<double> genCharginoEta;
+      std::vector<double> genCharginoPhi;
+      std::vector<double> genCharginoEnergy;
 
       virtual void beginRun(edm::Run const&, edm::EventSetup const&);
       virtual void endRun(edm::Run const&, edm::EventSetup const&);
